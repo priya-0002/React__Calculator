92a80fbc45abb34579d07fd143156e189c19dccb
priya-002 <priya.md0002@gmail.com>
2021-05-07 19:25:44 +0530

    first commit

--- src/cal/Cal.js
+++ src/cal/Cal.js
@@ -1,40 +1,33 @@
 import React from 'react'
-import {useState}from 'react'
+import {useState} from 'react'
 const Cal = () => {
-    const [input1, setInput1] = useState(null)
-    const [input2, setInput2] = useState(null)
-    const [res, setRes] = useState('')
+    const [input1, setInput1] = useState(0)
+    const [input2, setInput2] = useState(0)
+    const [res, setRes] = useState(0)
     const text1 =(e)=>{
         setInput1(e.target.value)
-        console.log('1',setInput1)
+        console.log(typeof input1)
 
     }
     const text2 =(e)=>{
         setInput2(e.target.value)
-        console.log('2',setInput2)
+        console.log(typeof input2)
 
     }
     const add =(e)=>{
         setRes(input1+input2)
+        console.log(typeof res)
 
     }
     return (
         <div>
             <div className="parent">
-                <input type="text" onChange={text1} />
-                <input type="text" onChange={text2} />
-                <input type="text" onClick={add} />
+                <input type="number" onChange={text1} />
+                <input  type="number" onChange={text2} />
                 
-                <button>+</button>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
+                
+                <button onClick={add}>=</button>
+              
             </div>
         </div>
     )
