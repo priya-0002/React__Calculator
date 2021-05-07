a49849da1ea2ac8fb6b81539040362f292cdb877
priya-002 <priya.md0002@gmail.com>
2021-05-07 13:20:57 +0530

    first commit

--- src/cal/Cal.js
+++ src/cal/Cal.js
@@ -3,12 +3,19 @@ import {useState}from 'react'
 const Cal = () => {
     const [input1, setInput1] = useState(null)
     const [input2, setInput2] = useState(null)
+    const [res, setRes] = useState('')
     const text1 =(e)=>{
         setInput1(e.target.value)
+        console.log('1',setInput1)
 
     }
     const text2 =(e)=>{
         setInput2(e.target.value)
+        console.log('2',setInput2)
+
+    }
+    const add =(e)=>{
+        setRes(input1+input2)
 
     }
     return (
@@ -16,8 +23,9 @@ const Cal = () => {
             <div className="parent">
                 <input type="text" onChange={text1} />
                 <input type="text" onChange={text2} />
+                <input type="text" onClick={add} />
                 
-                
+                <button>+</button>
                 <button>1</button>
                 <button>2</button>
                 <button>3</button>
