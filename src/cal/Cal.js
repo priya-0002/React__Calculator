24da0f0ae48af921b2df903d5d0d1482a99ea49a
priya-002 <priya.md0002@gmail.com>
2021-05-08 22:35:40 +0530

    first commit

--- src/cal/Cal.js
+++ src/cal/Cal.js
@@ -1,37 +1,103 @@
 import React from 'react'
 import {useState} from 'react'
 const Cal = () => {
-    const [input1, setInput1] = useState(0)
-    const [input2, setInput2] = useState(0)
-    const [res, setRes] = useState(0)
+    const [input1, setInput1] = useState()
+    const [input2, setInput2] = useState()
+    const [res, setRes] = useState()
+   
     const text1 =(e)=>{
         setInput1(e.target.value)
-        console.log(typeof input1)
+       
 
     }
     const text2 =(e)=>{
         setInput2(e.target.value)
-        console.log(typeof input2)
+       
 
     }
     const add =(e)=>{
         const x=Number(input1)
         const y=Number(input2)
        const addition=x+y
-        setRes(addition)
+       setRes(addition)
+       
+       
 
-        console.log(res)
+     
 
     }
+    const subtract =(e)=>{
+        const x=Number(input1)
+        const y=Number(input2)
+       const minus=x-y
+        setRes(minus)
+    }
+        
+    const multiply =(e)=>{
+        const x=Number(input1)
+        const y=Number(input2)
+       const cross=x*y
+        setRes(cross)
+
+       
+    }
+    const divide =(e)=>{
+        const x=Number(input1)
+        const y=Number(input2)
+       const div=x/y
+        setRes(div)
+       
+    }
+    const remainder =(e)=>{
+        const x=Number(input1)
+        const y=Number(input2)
+       const rem=x%y
+        setRes(rem)
+       
+    }
+
     return (
         <div>
             <div className="parent">
                 <input type="number" onChange={text1} />
+                <button>+</button>
                 <input  type="number" onChange={text2} />
-                
-                
-                <button onClick={add}>=</button>
+                 <button onClick={add}>=</button> <p>{res}</p>
+               
+               
+                <br />
+
+                <input type="number" onChange={text1} />
+                <button>-</button>
+                <input  type="number" onChange={text2} />
+                <button onClick={subtract}>=</button> <p>{res}</p>
+               
+               
+
+                <br />
+
+                <input type="number" onChange={text1} />
+                <button>*</button>
+                <input  type="number" onChange={text2} />
+                <button onClick={multiply}>=</button> <p>{res}</p>
               
+               
+               <br />
+
+               <input type="number" onChange={text1} />
+                <button>/</button>
+                <input  type="number" onChange={text2} />
+                <button onClick={divide}>=</button>  <p>{res}</p>
+              
+
+                <br />
+
+                <input type="number" onChange={text1} />
+                <button>%</button>
+                <input  type="number" onChange={text2} />
+                <button onClick={ remainder}>=</button>  <p>{res}</p>
+                
+               
             </div>
         </div>
     )
